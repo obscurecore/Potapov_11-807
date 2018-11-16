4e5f57fb59099e0be5a9d0d391f7eb44f6d1c873
obscurecore <43168898+obscurecore@users.noreply.github.com>
2018-11-16 11:04:29 +0300

    Update Main.java

--- HW15/Main.java
+++ HW15/Main.java
@@ -1,12 +1,10 @@
-package ru.kpfu.itis.textsimilarity;
-
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         TextAnalyzer analyzer = new JacardTextAnalayzer();
-        System.out.println("Откуда считываем значения?\nКонсоль - 1. Файл - 2");
+        System.out.println("Консоль - 1. Файл - 2");
         Scanner sc = new Scanner(System.in);
         int params = sc.nextInt();
         switch (params) {
