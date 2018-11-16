08da6cbaeb70321b5b6195dc0c1caff86f0a4378
obscurecore <43168898+obscurecore@users.noreply.github.com>
2018-11-16 15:08:15 +0300

    Update ConsolReader.java

--- HW15/ConsolReader.java
+++ HW15/ConsolReader.java
@@ -6,21 +6,12 @@ public class ConsolReader {
     TextAnalyzer analyzer = new JacardTextAnalayzer();
     TextAnalyzer cosAnalyzer = new CosMethod();
     Scanner sc = new Scanner(System.in);
-    {
-        System.out.println("Первый текст");
-    }
-
     String consolWord1 = sc.nextLine();
-
-    {
-        System.out.println("Второй текст");
-    }
-
     String consolWord2 = sc.nextLine();
     double coef = analyzer.analyze(new SimpleTextProvider(consolWord1), new SimpleTextProvider(consolWord2));
     double cosCoef = cosAnalyzer.analyze(new SimpleTextProvider(consolWord1), new SimpleTextProvider(consolWord2));
     {
-        System.out.println("Коэффициент схожести по Джакарду: " + coef);
-        System.out.println("Косинусова мера: "+cosCoef);
+        System.out.println("Джакард: " + coef);
+        System.out.println("мера: "+cosCoef);
     }
 }
